da68e275dfbf41db51874e3b1ebbce20981b1062
nicholas Mawira <22701258-nicholasMawira@users.noreply.replit.com>
2025-04-01 04:26:57 +0000

    ai_bot

---
+++ ai_bot.py
@@ -0,0 +1,13 @@
+import google.generativeai as genai
+import sys
+import os
+my_secret = os.environ['secret_key']
+
+genai.configure(api_key=my_secret)
+def gen_response(prompt):
+  model = genai.GenerativeModel("gemini-1.5-pro")
+  response = model.generate_content(prompt)
+  print(response.text)
+
+if __name__ == "__main__":
+  gen_response(sys.argv[1])
